6755566dd1c3875bf5553172749df6e4350890dc
maigutter <maigutter@gmail.com>
2023-09-15 19:33:06 -0300

    this is me being slightly obsessive with the colors and changing the cartWidget colors to be on brand

--- src/Components/Navbar/Cart/CartItems.jsx
+++ src/Components/Navbar/Cart/CartItems.jsx
@@ -1,10 +1,12 @@
 const CartItems = ({ count = 0 }) => {
   return (
     <div
-      className="position-absolute bg-primary rounded-circle px-2 "
+      className="position-absolute brandColorPink rounded-circle px-2 "
       style={{ top: 0, right: 0 }}
     >
-      <span style={{ fontSize: "10px", color: "white" }}>{count}</span>
+      <span style={{ fontSize: "10px", color: "black", fontWeight: "bold" }}>
+        {count}
+      </span>
     </div>
   );
 };
