20c8eee9d8e09ae17f2c1a0f0d6a272b69972f1e
maigutter <maigutter@gmail.com>
2023-09-15 12:16:30 -0300

    solving the image importation problem

--- src/Components/Navbar/Brand.jsx
+++ src/Components/Navbar/Brand.jsx
@@ -1,8 +1,8 @@
-import brandImage from "../../assets/logo.png.PNG";
+import brandImage from "../../assets/logo.png";
 const Brand = () => {
   return (
     <a className="navbar-brand" href="#">
-      <img src={brandImage} alt="logo.MaiLanguageLab.1" />
+      <img src={brandImage} alt="logo.MaiLanguageLab.1" className="iconImage" />
     </a>
   );
 };
