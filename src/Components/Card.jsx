2418ca818f0d887f3ce96ef4f64ede615fcd44d7
maigutter <maigutter@gmail.com>
2023-10-04 02:28:24 -0300

    polishing details

---
+++ src/Components/Card.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+function CardShop(Key, Name, Category, Description) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="logo"
+        height="140"
+        image="../assets/logo3.jpeg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {Name}
+        </Typography>
+        <Typography gutterBottom variant="h6" component="div">
+          {Category}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {Description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Buy</Button>
+        <Link to={`/coursesShop/${Key}`}>Learn More</Link>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default CardShop;
--- src/layouts/CoursesShop.jsx
+++ src/layouts/CoursesShop.jsx
@@ -1,11 +1,21 @@
 import ItemListContainer from "../Components/ItemListContainer";
 import PageTitle from "../Components/PageTitle";
+import getCourses from "../CoursesArray";
+import CardShop from "../Components/Card";
 
 function CoursesShop() {
   return (
     <>
       <PageTitle title="Cursos" />
       <ItemListContainer greeting="You can find the different courses to buy here" />
+      {getCourses.map((course) => (
+        <CardShop
+          Key={course.id}
+          Name={course.title}
+          Category={course.category}
+          Description={course.description}
+        />
+      ))}
     </>
   );
 }
