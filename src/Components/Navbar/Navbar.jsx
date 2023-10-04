7a471d1f6005a679f9fa9273c7b1548770fd762f
maigutter <maigutter@gmail.com>
2023-10-04 00:52:45 -0300

    taking categoryItem out of navbar because it's what I was using before to make the navbar clickable but actually didn't make sense with the route path and it was clashing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import Brand from "./Brand";
-import CategoryItem from "./CategoryItem";
+//import CategoryItem from "./CategoryItem";
 import MenuButton from "./MenuButton";
 import CartWidget from "./Cart/CartWidget";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Navbar_Routes from "../../routes/Routes";
 
 const Navbar = () => {
   return (
@@ -12,15 +13,18 @@ const Navbar = () => {
         <MenuButton />
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <CategoryItem isActive={true} name="Cursos" />
-            </li>
-            <li className="nav-item">
-              <CategoryItem isActive={false} name="Clases sueltas" />
-            </li>
-            <li className="nav-item">
-              <CategoryItem isActive={false} name="Sobre nosotres" />
-            </li>
+            {Navbar_Routes.map((route) => (
+              <li key={route.path} className="nav-item">
+                <NavLink
+                  to={route.path}
+                  className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""
+                  }
+                >
+                  {route.name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
           <CartWidget />
         </div>
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+const Navbar_Routes = [
+  {
+    path: "/",
+    name: "Inicio",
+  },
+  {
+    path: "/coursesShop",
+    name: "Shop",
+  },
+  {
+    path: "/oneOnOneClasses",
+    name: "One on One Classes",
+  },
+  {
+    path: "/contacto",
+    name: "Contacto",
+  },
+];
+
+export default Navbar_Routes;
