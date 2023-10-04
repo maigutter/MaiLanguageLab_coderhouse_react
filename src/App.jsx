d7b293e74b10479fe49a5286e5025f8930d5a4ed
maigutter <maigutter@gmail.com>
2023-10-03 23:20:51 -0300

    commiting changes for the second delivery

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import GeneralTitle from "./Components/GeneralTitle";
 import ItemListContainer from "./Components/ItemListContainer";
 import Navbar from "./Components/Navbar/Navbar";
@@ -6,12 +7,24 @@ import PageTitle from "./Components/PageTitle";
 
 function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <Navbar />
       <GeneralTitle />
       <PageTitle title="Cursos" />
       <ItemListContainer greeting="You can find the different courses to buy here" />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<h1>HOME</h1>} />
+        <Route exact path="/pokemons" element={<Pokemons />} />
+        <Route exact path="/pokemon/:pokemonName" element={<PokemonByName />} />
+
+        <Route
+          path="*"
+          element={
+            <h2>¡Lo sentimos! No pudimos encontrar lo que estás buscando.</h2>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
