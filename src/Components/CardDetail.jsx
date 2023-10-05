5242720cbbcc2bb299f8f844d09ac1b33363dfb0
maigutter <maigutter@gmail.com>
2023-10-05 14:01:33 -0300

    editing more css and centering and etc

--- src/Components/CardDetail.jsx
+++ src/Components/CardDetail.jsx
@@ -5,25 +5,26 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-function CardShopDetail({ id, Name, Category, Description }) {
+function CardShopDetail({ Name, Category, Description }) {
   return (
-    <Card sx={{ maxWidth: 465 }}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {Name}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div">
-          {Category}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {Description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Buy</Button>
-        <Link to={"/coursesShop/" + id}>Learn More</Link>
-      </CardActions>
-    </Card>
+    <div className="center">
+      <Card sx={{ maxWidth: 465 }}>
+        <CardContent className="center">
+          <Typography gutterBottom variant="h4" component="div">
+            {Name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {Category}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {Description}
+          </Typography>
+        </CardContent>
+        <CardActions className="center">
+          <Button size="medium">Buy</Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
